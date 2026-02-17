e1122a1b999b595628d75af0393d5d1e9d3e20b2
Adeel <iamadeelchaudhary@gmail.com>
2026-02-17 20:43:49 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Mobile menu toggle
+const btn = document.getElementById("mobile-btn");
+const menu = document.getElementById("mobile-menu");
+btn.addEventListener("click", () => menu.classList.toggle("hidden"));
